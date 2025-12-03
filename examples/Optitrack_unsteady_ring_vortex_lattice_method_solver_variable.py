6772876f76f23cb6df9ba58626bd9cd4b51f91e5
henridesjonqueres <henri.desjonq@gmail.com>
2025-12-03 17:22:17 -0500

    Update airfoil_creation and differential_measures

--- examples/Optitrack_unsteady_ring_vortex_lattice_method_solver_variable.py
+++ examples/Optitrack_unsteady_ring_vortex_lattice_method_solver_variable.py
@@ -1,8 +1,16 @@
-
+"""
+This example shows how to use the UnsteadyRingVortexLatticeMethodSolver withOptiTrack data
+"""
 
 import pterasoftware as ps
 
-optitrack_file = r"C:\Users\henri\Documents\MIT\Travail\Optitrack\Test_2\trois_bis_hz.csv"
+# Define all the necessary parameters for loading the OptiTrack data.
+
+
+optitrack_file = r"C:\Users\henri\Documents\MIT\Travail\Optitrack\Test_2\trois_hz.csv"
+
+# Don't forget to add the Z1 and Z2 trackers, which are virtual trackers defining the wing root chord line. 
+# Their definition is in airfoil_creation.py adapted for our robot, you may need to change the position according to your setup.
 list_trackers = [
     "Z1", "Z2", "A1", "A2", "A3", "B1", "B2", "B3", "B4", "B5",
     "C1", "C2", "C3", "C4", "C5", "D1", "D2", "D3", "D4", "D5",
@@ -10,11 +18,15 @@ list_trackers = [
     "G1", "G2", "G3", "G4", "G5", "H1", "H2", "H3", "H4",
     "I1", "I2"
 ]
+
 columns = ps.geometry.airfoil_creation.extract_columns(list_trackers)
+
 Nb_columns=len(columns)
 
-data = ps.geometry.airfoil_creation.load_data(optitrack_file, list_trackers) 
+# Load the OptiTrack data.
+data = ps.geometry.airfoil_creation.load_data(optitrack_file, list_trackers, right = False) *10**-3  # Convert from mm to m
 
+# Create the airfoils at step 0.
 airfoils_0 = {}
 
 for column in columns:
@@ -25,37 +37,38 @@ for column in columns:
         list_trackers=list_trackers,
     )
 
+# Define the airplane geometry using the airfoils created from the OptiTrack data.
 example_airplane = ps.geometry.airplane.Airplane(
     wings=[
         ps.geometry.wing.Wing(
             wing_cross_sections=[
                 ps.geometry.wing_cross_section.WingCrossSection(
-                    num_spanwise_panels = None if column == columns[-1] else 1,
+                    num_spanwise_panels = None if column == columns[-1] else 1, # Last wing cross-section has no panels 
                     chord=airfoils_0[column].get_chord_length(), 
                     Lp_Wcsp_Lpp=airfoils_0[column].get_position()[0],
                     angles_Wcsp_to_Wcs_ixyz=airfoils_0[column].get_position()[1],
                     control_surface_symmetry_type="symmetric",
                     control_surface_hinge_point=0.75,
                     control_surface_deflection=0.0,
-                    spanwise_spacing=None if column == columns[-1] else "uniform",
+                    spanwise_spacing=None if column == columns[-1] else "uniform", # Last wing cross-section has no panels
                     airfoil=ps.geometry.airfoil.Airfoil(
                         name=f"column_{column}_airfoil",  
                         outline_A_lp=airfoils_0[column].get_airfoil_shape(),
                         resample=True,
                         n_points_per_side=400,
-                        data=data,
-                        column=column,
-                        list_trackers=list_trackers,
+                        data=data,    # Pass the data to the Airfoil
+                        column=column,        # Pass the column to the Airfoil
+                        list_trackers=list_trackers,       # Pass the list of trackers to the Airfoil
                     ),
                 )
                 for column in columns
             ],
             name="Main Wing",
-            Ler_Gs_Cgs=(0.0, 0.5, 0.0),
+            Ler_Gs_Cgs=(0.0, 0.0005, 0.0),
             angles_Gs_to_Wn_ixyz=(0.0, 0.0, 0.0),
             symmetric=True,
             mirror_only=False,
-            symmetryNormal_G=(0.0, 1.0, 0.0),
+            symmetryNormal_G=(0.0, 0.0001, 0.0),
             symmetryPoint_G_Cg=(0.0, 0.0, 0.0),
             num_chordwise_panels=6,
             chordwise_spacing="uniform",
@@ -69,6 +82,9 @@ example_airplane = ps.geometry.airplane.Airplane(
     c_ref=None,
     b_ref=None,
 )
+
+
+# Define the airplane's AirplaneMovement.
 main_wing_cross_section_movement=[None]*Nb_columns
 
 for i in range (Nb_columns):
@@ -83,7 +99,7 @@ for i in range (Nb_columns):
             periodAngles_Wcsp_to_Wcs_ixyz=(0.0, 0.0, 0.0),
             spacingAngles_Wcsp_to_Wcs_ixyz=("sine", "sine", "sine"),
             phaseAngles_Wcsp_to_Wcs_ixyz=(0.0, 0.0, 0.0),
-            optitrack = True
+            optitrack = True  # Put to True to use OptiTrack data. The other parameters will be ignored except for base_wing_cross_section
         )
     )
 
@@ -101,11 +117,11 @@ for i in range (Nb_columns):
             periodAngles_Wcsp_to_Wcs_ixyz=(0.0, 0.0, 0.0),
             spacingAngles_Wcsp_to_Wcs_ixyz=("sine", "sine", "sine"),
             phaseAngles_Wcsp_to_Wcs_ixyz=(0.0, 0.0, 0.0),
-            optitrack = True
+            optitrack = True  # Put to True to use OptiTrack data. The other parameters will be ignored except for base_wing_cross_section
         )
     )
 
-
+# Define the airplane's WingMovements. It's necessary but the simulation won't use it.
 main_wing_movement = ps.movements.wing_movement.WingMovement(
     base_wing=example_airplane.wings[0],
     wing_cross_section_movements= main_wing_cross_section_movement,
@@ -113,8 +129,8 @@ main_wing_movement = ps.movements.wing_movement.WingMovement(
     periodLer_Gs_Cgs=(0.0, 0.0, 0.0),
     spacingLer_Gs_Cgs=("sine", "sine", "sine"),
     phaseLer_Gs_Cgs=(0.0, 0.0, 0.0),
-    ampAngles_Gs_to_Wn_ixyz=(15.0, 0.0, 0.0),  # (0.0, 0.0, 0.0),
-    periodAngles_Gs_to_Wn_ixyz=(1.0, 0.0, 0.0),  # (0.0, 0.0, 0.0),
+    ampAngles_Gs_to_Wn_ixyz=(0.0, 0.0, 0.0), 
+    periodAngles_Gs_to_Wn_ixyz=(0.0, 0.0, 0.0), 
     spacingAngles_Gs_to_Wn_ixyz=("sine", "sine", "sine"),
     phaseAngles_Gs_to_Wn_ixyz=(0.0, 0.0, 0.0),
 )
@@ -125,13 +141,13 @@ reflected_main_wing_movement = ps.movements.wing_movement.WingMovement(
     periodLer_Gs_Cgs=(0.0, 0.0, 0.0),
     spacingLer_Gs_Cgs=("sine", "sine", "sine"),
     phaseLer_Gs_Cgs=(0.0, 0.0, 0.0),
-    ampAngles_Gs_to_Wn_ixyz=(15.0, 0.0, 0.0),  # (0.0, 0.0, 0.0),
-    periodAngles_Gs_to_Wn_ixyz=(1.0, 0.0, 0.0),  # (0.0, 0.0, 0.0),
+    ampAngles_Gs_to_Wn_ixyz=(0.0, 0.0, 0.0),  
+    periodAngles_Gs_to_Wn_ixyz=(0.0, 0.0, 0.0), 
     spacingAngles_Gs_to_Wn_ixyz=("sine", "sine", "sine"),
     phaseAngles_Gs_to_Wn_ixyz=(0.0, 0.0, 0.0),
 )
 
-# Now define the example airplane's AirplaneMovement.
+# Now define the example airplane's AirplaneMovement. For now, no movement of the airplane is possible.  
 airplane_movement = ps.movements.airplane_movement.AirplaneMovement(
     base_airplane=example_airplane,
     wing_movements=[main_wing_movement, reflected_main_wing_movement],
@@ -145,37 +161,29 @@ airplane_movement = ps.movements.airplane_movement.AirplaneMovement(
     phaseAngles_E_to_B_izyx=(0.0, 0.0, 0.0),
 )
 
-# Delete the extraneous pointers to the WingMovements.
-del main_wing_movement
-del reflected_main_wing_movement
+
 
 # Define a new OperatingPoint.
 example_operating_point = ps.operating_point.OperatingPoint(
-    rho=1.225, vCg__E=1000.0, alpha=1.0, beta=0.0, externalFX_W=0.0, nu=15.06e-6
+    rho=1.225, vCg__E=1.0, alpha=1.0, beta=0.0, externalFX_W=0.0, nu=15.06e-6
 )
 
 # Define the operating point's OperatingPointMovement.
 operating_point_movement = ps.movements.operating_point_movement.OperatingPointMovement(
-    base_operating_point=example_operating_point, periodVCg__E=0.0, spacingVCg__E="sine"
-)
-
-# Delete the extraneous pointer.
-del example_operating_point
+    base_operating_point=example_operating_point)
 
 # Define the Movement. This contains the AirplaneMovement and the
 # OperatingPointMovement.
 movement = ps.movements.movement.Movement(
     airplane_movements=[airplane_movement],
     operating_point_movement=operating_point_movement,
-    delta_time=1/360,
+    delta_time=1/360,   # Time step between two frames in the OptiTrack data
     num_cycles=None,
     num_chords=None,
-    num_steps=300,
+    num_steps=100,
 )
 
-# Delete the extraneous pointers.
-del airplane_movement
-del operating_point_movement
+
 
 # Define the UnsteadyProblem.
 example_problem = ps.problems.UnsteadyProblem(
@@ -192,39 +200,36 @@ example_solver = (
     )
 )
 
-# Delete the extraneous pointer.
-del example_problem
 
 # Run the solver.
 example_solver.run(
     logging_level="Warning",
     prescribed_wake=True,
 )
+
+
 # ps.output.plot_results_versus_time(
 #     unsteady_solver=example_solver,
 #     show=True,
 #     save=False,
 # )
 
-# ps.output.export_data(
-#         solver = example_solver, 
-#         filename = "panel_vertices_example_airplane_5.csv")
 
 # Call the animate function on the solver. This produces a GIF of the wake being
 # shed. The GIF is saved in the same directory as this script. Press "q",
 # after orienting the view, to begin the animation.
 
-analysis = ps.differential_measures.Analysis(example_solver,3)
-
 # ps.output.animate(
-#     unsteady_solver=analysis.fake_solver,
+#     unsteady_solver=example_solver,
 #     scalar_type="lift",
 #     show_wake_vortices=True,
 #     save=True,
-#     track_point=(0, 0.3, 0.7)
 # )
 
-analysis.plot_section(4, compare_fake=False)
+# You can creat a simulated airplane with the same geometry by calling differential_measures.Analysis. 
+# You will be able to use output for this new simulation and compare the results with the OptiTrack based simulation.
+
+# analysis = ps.differential_measures.Analysis(example_solver,3)
 
 
 
